d89e00bb83810ecb67cbb52ed8267f531d539a40
n8shaw <119529291+n8shaw@users.noreply.github.com>
2023-12-04 22:35:55 +0000

    attempted ish

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const fs = require('fs');
 
 let path = require('path');
 
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.urlencoded({ extended: true }));
+
 
 const knex = require('knex')({
 client: 'pg',
@@ -13,17 +22,11 @@ connection: {
     host: process.env.RDS_HOSTNAME || 'localhost',
     user: process.env.RDS_USERNAME || 'test',
     password: process.env.RDS_PASSWORD || 'test',
-    database: process.env.RDS_DB_NAME || 'intex',
+    database: process.env.RDS_DB_NAME || 'temp',
     port: process.env.RDS_PORT || 5432
 }
 });
 
-
-const port = process.env.PORT || 3000;
-
-let app = express();
-
-app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {  
     res.render('landing');
@@ -49,7 +52,7 @@ app.post("/login", (req, res) => {
         }
         else
         {
-            alert("Invaldi Credentials");
+            alert("Invalid Credentials");
             res.render("/login")
 
         }
