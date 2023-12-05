84f6656ef1cb20deaecf9ab12e49d59ad48aa45d
Zach Bagley <szachbagley@gmail.com>
2023-12-04 18:14:22 -0700

    modified login page

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ app.post("/login", (req, res) => {
         }
         else
         {
-            alert("We couln't find your account. Please check that your username and password are typed correctly and that your account exists (only current users can create new user accounts).")
+            //"alert" doesn't work on the node app i guess
+            //alert("We couln't find your account. Please check that your username and password are typed correctly and that your account exists (only current users can create new user accounts).");
             res.render("login");
         }
     })
