d9150c75fc66636542a7ec657aa9f0d0f432bd55
broncogooch <120050059+broncogooch@users.noreply.github.com>
2023-12-05 23:53:49 +0000

    added params

--- index.js
+++ index.js
@@ -165,10 +165,13 @@ app.get('/report', (req, res) => {
         .join('gender', 'main.gender_id', '=', 'gender.gender_id')
         .join('occupation', 'main.occupation_status_id', '=', 'occupation.occupation_status_id')
         .join('relationship', 'main.relationship_status_id', '=', 'relationship.relationship_status_id')
-        .where('main.entry_id', req.body.entry_id_filter)
+        .where('main.entry_id', req.params.entry_id_filter)
         .then( allSurveys => {
             if (allSurveys.length) {
-            res.render('report', {mySurveys : allSurveys})
+            res.render('report', {mySurveys : allSurveys});
+            }
+            else {
+                res.render('nodata');
             }
         });
     } else {
