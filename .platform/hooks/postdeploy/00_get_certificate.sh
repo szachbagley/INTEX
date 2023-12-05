8dbf54e39cb11a57d68e78d719b5ac263208b327
Eli Baker <eliasjonathonbaker@gmail.com>
2023-12-05 12:41:09 -0700

    fixing security certificate

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d group3-5.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email nathanjshaw88@gmail.com
+sudo certbot -n -d provohealthandwellness.is404.net --nginx --agree-tos --email nathanjshaw88@gmail.com
