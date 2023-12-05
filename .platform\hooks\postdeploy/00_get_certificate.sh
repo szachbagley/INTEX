e30e5dae4554202dc8f99ce664f565a37c1ba517
n8shaw <119529291+n8shaw@users.noreply.github.com>
2023-12-05 14:59:51 +0000

    updated certificate

--- .platform\hooks\postdeploy/00_get_certificate.sh
+++ .platform\hooks\postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d sec3group5-2023.is404.net --nginx --agree-tos --email nathanjshaw88@gmail.com
+sudo certbot -n -d group3-5.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email nathanjshaw88@gmail.com
