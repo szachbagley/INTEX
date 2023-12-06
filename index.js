60346ce16b3c146b2cdb88759011a9cd34230ccd
broncogooch <120050059+broncogooch@users.noreply.github.com>
2023-12-06 17:12:31 +0000

    its eli

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ connection: {
     user: process.env.RDS_USERNAME || 'test',
     password: process.env.RDS_PASSWORD || 'test',
     database: process.env.RDS_DB_NAME || 'intex',
-    port: process.env.RDS_PORT || 5432,
+    port: process.env.RDS_PORT || 5432 || 5433,
     ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
 }
 });
