1a77cf6a7cc4cedef5ac76bfe1f5573d84f23a5f
broncogooch <120050059+broncogooch@users.noreply.github.com>
2023-12-06 00:10:03 +0000

    uggghhhh

--- index.js
+++ index.js
@@ -163,13 +163,14 @@ app.get('/report', (req, res) => {
   app.get('/report/:entry_id_filter', (req, res) => {
     console.log(req.session.account);
     if (req.session.account) {
+        let parsedNum = parseInt(req.params.entry_id_filter);
         knex.select().from('main')
         .join('average_time', 'main.average_time_id', '=', 'average_time.average_time_id')
         .join('city', 'main.city_id', '=', 'city.city_id')
         .join('gender', 'main.gender_id', '=', 'gender.gender_id')
         .join('occupation', 'main.occupation_status_id', '=', 'occupation.occupation_status_id')
         .join('relationship', 'main.relationship_status_id', '=', 'relationship.relationship_status_id')
-        .where('main.entry_id', req.params.entry_id_filter)
+        .where('main.entry_id', parsedNum)
         .then( allSurveys => {
             if (allSurveys.length) {
             res.render('report', {mySurveys : allSurveys});
