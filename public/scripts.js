5ac11c22d566a4ca2592cff556a742a0f167b140
Zach Bagley <szachbagley@gmail.com>
2023-12-07 14:40:17 -0700

    mobile friendly homepage 2:40pm

---
+++ public/scripts.js
@@ -0,0 +1,29 @@
+function changeNav() {
+    let navBar = document.getElementById('navBar');
+    if (window.matchMedia("(max-width: 1000px)").matches) {
+        // If the screen size is 1000px or under, change the class of the element
+        navBar.classList.remove('fullPageNav');
+        navBar.classList.add('mobileNav')
+        navBar.classList.add('hidden')
+    } else {
+        // If the screen size is over 1000px, change the class of the element
+        navBar.classList.remove('mobileNav');
+        navBar.classList.remove('hidden');
+        navBar.classList.remove('visible')
+        navBar.classList.add('fullPageNav');
+    }
+    console.log(navBar.classList);
+}
+
+function toggle() {
+    if (window.matchMedia("(max-width: 1000px)").matches) {
+        let navBar = document.getElementById('navBar');
+        if (navBar.classList.contains('hidden')) {
+            navBar.classList.remove('hidden');
+            navBar.classList.add('visible');
+        } else {
+            navBar.classList.remove('visible');
+            navBar.classList.add('hidden');
+        }
+    }
+}
