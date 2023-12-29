010be490c9e80c7ac73c4688b9b6ef6042efd1af
Zach Bagley <szachbagley@gmail.com>
2023-12-29 16:11:38 -0700

    ssl

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d zachintex2023.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email szachbagley@gmail.com
+sudo certbot -n -d provomentalhealth.com --nginx --agree-tos --email szachbagley@gmail.com
